c6941ed5f0cb965c8fc2d2f95ca9dbd00791d1e7
Robert Ni <robertni@outlook.fr>
2021-02-04 12:23:56 -0500

    more optimized code

--- lab1/HelloWorld/app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ lab1/HelloWorld/app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,31 +3,24 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    // initialize objects
-    Button myBtn;
-    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // define what the buttons point to
-        myBtn = (Button)findViewById(R.id.button);
-        text = (TextView)findViewById(R.id.textView);
-
-        // if button is clicked, do this
-        myBtn.setOnClickListener(new View.OnClickListener() {
+        // defines what happens when button is clicked
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                // sets text for object with id textView
-                text.setText("You have clicked the button");
+            public void onClick(View v) {
+                // sets text of textView
+                ((TextView)findViewById(R.id.textView)).setText("You have clicked the button");
             }
         });
+
     }
+
 }
